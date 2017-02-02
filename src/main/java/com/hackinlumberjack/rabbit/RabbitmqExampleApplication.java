332386ebdf1629a6f75f566885613931dfbe5ffc
Erik <eculberson12@gmail.com>
2017-02-02 14:53:21 -0500

    Adding Beans that will be used to set up rabbitmq

--- src/main/java/com/hackinlumberjack/rabbit/RabbitmqExampleApplication.java
+++ src/main/java/com/hackinlumberjack/rabbit/RabbitmqExampleApplication.java
@@ -2,6 +2,11 @@ package com.hackinlumberjack.rabbit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
 
 @SpringBootApplication
 public class RabbitmqExampleApplication {
@@ -9,4 +14,19 @@ public class RabbitmqExampleApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RabbitmqExampleApplication.class, args);
 	}
+
+  @Bean
+  public Queue queue() {
+    return new Queue("simple.queue");
+  }
+
+  @Bean
+  public DirectExchange exchange() {
+    return new DirectExchange("simple.exchange");
+  }
+
+  @Bean
+  public Binding binding(DirectExchange exchange, Queue queue) {
+    return BindingBuilder.bind(queue).to(exchange).with("simple.excahnge");
+  }
 }
